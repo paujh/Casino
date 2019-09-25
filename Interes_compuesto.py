a400fdd0017a3c4583d407e16fa9eb6148833be5
paujh <48219253+paujh@users.noreply.github.com>
2019-09-25 10:44:00 -0500

    Create Interes_compuesto.py

---
+++ Interes_compuesto.py
@@ -0,0 +1,10 @@
+#Programa que calcula el interes compuesto 
+#Primero le pediremos todos los datos necesarios al usuario
+Capital = float(input("Ingrese el capital con el que cuenta: "))
+Tiempo = int(input("Ingrese los años que tendra invertido el capital: "))
+i_anual = float(input("Ingrese el interes que usara, en porcentaje: "))
+#Ahora haremos que se calcule el interes por año y que muestre la ganancia, año por año.
+interes = 0.0
+for i in range (Tiempo+1):
+  interes = Capital*((1+(i_anual/100))**i)
+  print("Su ganancia en el año " + str(i) + " sera de " + str(interes))
